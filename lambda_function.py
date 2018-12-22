bc7aa93a0c16737f68cc13ad8b55a81983e5f6cc
aagavin <aagavin@gmail.com>
2018-12-21 23:14:43 -0500

    csv parsing from google sheets

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,12 @@
-import json
+import csv
 import requests
+from io import StringIO
+from datetime import datetime, timedelta
 from cachecontrol import CacheControl
 
 MAP_URL = 'https://map.toronto.ca/geoservices/rest/search/rankedsearch'
 GIS_URL = 'https://gis.toronto.ca/arcgis/rest/services/primary/cot_geospatial21_mtm/MapServer/3/query'
+SHEET_URL = 'https://docs.google.com/spreadsheets/d/1Om0nwrYzeombeuMf-1pMksyG7oaTdXVpN3vR7-qrjdo/export?format=csv'
 
 query_gis = {
     'geometry': '',
@@ -13,8 +16,23 @@ query_gis = {
     'f': 'pjson'
 }
 
+date_map = {
+    'Monday': 0,
+    'Tuesday': 1,
+    'Wednesday': 2,
+    'Thursday': 3,
+    'Friday': 4,
+}
+
 s = requests.session()
 c_session = CacheControl(s)
+
+
+def get_end_of_week(area_day):
+    offset = date_map[area_day]
+    dt = datetime.now().date()
+    start = dt - timedelta(days=dt.weekday())
+    return (start + timedelta(days=offset)).strftime('%Y/%m/%d')
 
 
 def lambda_handler(event, context):
@@ -26,5 +44,13 @@ def lambda_handler(event, context):
     )
     query_gis['geometry'] = f'{long},{lat}'
     gis_r: dict = c_session.get(GIS_URL, params=query_gis).json()
-    area_name = gis_r['features'][0]['attributes']['AREA_NAME']
-    return area_name.replace(' ', '')
+    area_date = gis_r['features'][0]['attributes']['AREA_NAME'].replace(' ', '')
+    end_week = get_end_of_week(area_date[:-1])
+    gsheet = c_session.get(SHEET_URL).text
+    csv_dict = csv.DictReader(StringIO(gsheet))
+
+    for row in csv_dict:
+        if row["Calendar"] == area_date and row["WeekStarting"] == end_week:
+            val_tuple = row, csv_dict.__next__()
+    val_tuple
+    return ''
