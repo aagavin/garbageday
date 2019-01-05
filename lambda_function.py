804746db336b67db142324efaf5d4ee997e6e634
aagavin <aagavin@gmail.com>
2019-01-04 23:06:53 -0500

    removed cache

--- lambda_function.py
+++ lambda_function.py
@@ -6,14 +6,12 @@ import datetime
 from io import StringIO
 
 import requests
-from cachecontrol import CacheControl
 
 MAP_URL = 'https://map.toronto.ca/geoservices/rest/search/rankedsearch'
 GIS_URL = 'https://gis.toronto.ca/arcgis/rest/services/primary/cot_geospatial21_mtm/MapServer/3/query'
 SHEET_URL = 'https://docs.google.com/spreadsheets/d/1Om0nwrYzeombeuMf-1pMksyG7oaTdXVpN3vR7-qrjdo/export?format=csv'
 
-s = requests.session()
-c_session = CacheControl(s)
+session = requests.session()
 
 SMPT_PORT = 465  # For SSL
 SMPT_USERNAME = os.getenv('SMPT_USERNAME', '')
@@ -33,13 +31,13 @@ query_gis = {
 
 def get_collection_schedule(event) -> tuple:
     address: str = event['address']
-    map_r: dict = c_session.get(MAP_URL, params={'searchString': address}).json()
+    map_r: dict = session.get(MAP_URL, params={'searchString': address}).json()
     long, lat = (
         map_r['result']['bestResult'][0]['longitude'],
         map_r['result']['bestResult'][0]['latitude']
     )
     query_gis['geometry'] = f'{long},{lat}'
-    gis_r: dict = c_session.get(GIS_URL, params=query_gis).json()
+    gis_r: dict = session.get(GIS_URL, params=query_gis).json()
     area_date = gis_r['features'][0]['attributes']['AREA_NAME'].replace(' ', '')
     end_week = datetime.datetime.now().date().isocalendar()
     index = 1
